7a49d5ff6820a71e7cadc715a489465554c3e7dd
Princekap <kumahamezugbeprince@gmail.com>
2023-02-16 16:32:50 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main-entry
- * return always 0
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
