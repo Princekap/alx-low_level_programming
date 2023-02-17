4798f6b5812d1f5f541082409e57a715c22cd3c9
Princekap <kumahamezugbeprince@gmail.com>
2023-02-17 15:59:21 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphaBET
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	while
+		(a <= 'z') {
+			putchar(a);
+			a++;
+		}
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
