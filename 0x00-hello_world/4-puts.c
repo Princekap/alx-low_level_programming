6b17f4ffecfdb3e54b85be84b9ca582263189e07
Princekap <kumahamezugbeprince@gmail.com>
2023-02-17 07:03:11 -0800

    commit for correction on 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
