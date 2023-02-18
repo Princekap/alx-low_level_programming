238af9698ac129c014d95b37977d669440ed17bf
Princekap <kumahamezugbeprince@gmail.com>
2023-02-17 16:41:13 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Reverse alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char z = 'z';
+
+	while
+		(z >= 'a') {
+			putchar(z);
+			z--;
+		}
+	putchar('\n');
+	return (0);
+}
