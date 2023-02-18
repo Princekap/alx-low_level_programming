68d16585b0ba969e0c68a44d382bed86580ed825
Princekap <kumahamezugbeprince@gmail.com>
2023-02-17 16:59:52 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int b = 0;
+
+	while
+		(b < 10) {
+			putchar(b + '0');
+				b++;
+		}
+	while
+		(a <= 'f') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
