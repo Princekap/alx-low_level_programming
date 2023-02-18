53d54b0ee8df5bdf812cd828fea124566f2527f2
Princekap <kumahamezugbeprince@gmail.com>
2023-02-17 16:25:27 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while
+		(a < 10) {
+			printf("%d", a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
