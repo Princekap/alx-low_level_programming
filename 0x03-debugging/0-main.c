aceb368560ac5f7d63c82bf227fd6a9e59c77545
Princekap <kumahamezugbeprince@gmail.com>
2023-02-23 10:17:57 -0800

    Debugging qtn 1 correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+*Description:Debugging
+ * Return: Always 0 (success)
 */
 int main(void)
 {
         int i;
-
         i = 0;
         positive_or_negative(i);
 
