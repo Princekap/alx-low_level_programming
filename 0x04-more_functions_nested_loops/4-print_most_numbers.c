cc404cfec96adc9e91bd1d2062ff65628bcf3001
Princekap <kumahamezugbeprince@gmail.com>
2023-02-23 16:12:44 -0800

    print numbers and most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
  */
 void print_most_numbers(void)
 {
-int c;
+char c;
 
-for (c = 0; c <= 9; c++)
+for (c = '0'; c <= '9'; c++)
 {
-if (c != 50)
+if (c != '2')
 {
-if (c != 52)
+if (c != '4')
 {
 _putchar(c);
 }
