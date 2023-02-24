90940ef943a88e34e4c62fffe1f697fbbd0deb76
Princekap <kumahamezugbeprince@gmail.com>
2023-02-23 16:06:26 -0800

    print numbers and most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
  * Return: Always 0.
  */
 void print_numbers(void)
-{
-	char 'c';
+char 'c';
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
